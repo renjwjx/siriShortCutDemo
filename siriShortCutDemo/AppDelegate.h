ed855fbb0a332a9fb2e857dd23206b6b3bc728d4
renjinwei <renjwjx@gmail.com>
2018-10-17 15:46:03 +0800

    Initial Commit

---
+++ siriShortCutDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  siriShortCutDemo
+//
+//  Created by jinren on 10/17/18.
+//  Copyright © 2018 jinren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
